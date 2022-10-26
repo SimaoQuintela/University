e7c1ee095d0a67b258a7eefee41f75dfd306c1ed
SimaoQuintela <simaopedroquintela@hotmail.com>
2022-10-26 18:00:18 +0100

    Prob 5 update

--- TP1/Prob05/main.py
+++ TP1/Prob05/main.py
@@ -1,37 +1,52 @@
 import csv
 import re
+from json_write import write_on_json_file
 
 lines = []
 with open('./examples/alunos.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    spamreader = csv.reader(csvfile, delimiter='\n', quotechar='|')
     for row in spamreader:
-        lines.append(row)
+        lines.append(row[0])
 
-    
-print(lines)
-
-header = lines[0]
+line_with_header = lines[0]
 lines.pop(0)
 
-print(header)
-headers = {}
+
+headers = re.findall("([A-Za-z0-9úãóíáõé]+)+({((?:[0-9]+,?)+)})?", line_with_header)
+json_headers = {}
 i = 0
-for title in header:
-    match = re.match(r"([A-Za-z]+)({([0-9]+,?[0-9]*)})", title)
-    if match:
-        groups = match.groups()
-
-        headers[groups[0]] = {}
-
-
-        for j in range(int(groups[-1])):
-            index = groups[0] + f"[{j}]"
-            headers[groups[0]][index] = i
-            i+=1
-
-        print(match.groups())
-
-    else:
-        headers[i] = title
-        i+=1
 print(headers)
+for header in headers:
+    if "{" in header[1] and "}" in header[1]:
+        min_max_array = re.split(",", header[2])
+        if len(min_max_array) == 2:
+            min_array = int(min_max_array[0])
+            max_array = int(min_max_array[1])
+        else:
+            min_array = 1
+            max_array = int(min_max_array[0])
+        json_headers[i] = {}
+        json_headers[i][header[0]+header[1]] = (min_array,max_array)  
+         
+    else:
+        json_headers[i] = header[0]
+    i+=1
+
+print(json_headers)
+
+#print(headers)
+json_file_name = input("Nome do ficheiro JSON sem terminação: ") + ".json"
+f = open(json_file_name, "w")
+f.write("[\n")
+f.close()
+for (i,line) in enumerate(lines):
+    f = open(json_file_name, "a")
+    write_on_json_file(line, json_headers, f)
+    if i != len(lines)-1:
+        f.write("    },\n")
+    else:
+        f.write("    }")
+
+f = open(json_file_name, "a")
+f.write("\n]")
+f.close()
