8aed71802867ca0f41f992854df5e2a7e384e1f0
HugoRochartk <hugorochartk@gmail.com>
2022-12-29 00:01:01 +0000

    populate contact

--- Scripts/population/population.py
+++ Scripts/population/population.py
@@ -1,6 +1,9 @@
 import pandas as pd
+from faker import Faker
 from sys import argv
 from connection import create_db_connection, Error
+import random
+
 
 def execute_query(connection, query):
     cursor = connection.cursor()
@@ -10,6 +13,21 @@ def execute_query(connection, query):
         print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")
+
+
+
+def populate_contact(connection):
+    fake = Faker(["pt_PT"])
+
+    for i in range(1,31):
+        name = fake.name()
+        email = fake.email()
+        phnumber = "9"
+        for j in range(8):
+            phnumber += str(random.randint(0,9))
+
+        execute_query(connection, f"INSERT INTO contact (idContact, name, email, phone) VALUES ('{i}', '{name}', '{email}', '{phnumber}')")
+
 
 
 def read_query(connection, query):
@@ -27,6 +45,5 @@ def read_query(connection, query):
 connection = create_db_connection("localhost", "root", argv[1], "dorlux")
 
 
-execute_query(connection, "INSERT INTO address VALUES ('7', 'ASDAS', 'ASDDAS', 'ADSAD')")
-read_query(connection, "SELECT * FROM address")
+populate_contact(connection)
 
