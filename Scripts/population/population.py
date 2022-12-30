7b57e33ddae37ba1a79d48219e28078ce2c9d0da
Simao Quintela <simaopedroquintela@hotmail.com>
2022-12-30 19:26:34 +0000

    refactored code

--- Scripts/population/population.py
+++ Scripts/population/population.py
@@ -5,114 +5,81 @@ from sys import argv
 from connection import create_db_connection, Error
 import random
 from faker import Faker
-
+from query_actions import read_query, execute_query
 fake = Faker(["pt_PT"])
 
 # creating a connection
 connection = create_db_connection("localhost", "root", argv[1], "dorlux")
 
-def execute_query(connection, query):
-    cursor = connection.cursor()
-    print(query)
-    try:
-        cursor.execute(query)
-        connection.commit()
-        print("Query successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-
-
-def read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        
-        for entry in result:
-            print(entry)
-        
-        return result
-    except Error as err:
-        print(f"Error: '{err}'")
-
+def write_on_file(query):
+    f = open("population_script.txt", "a")
+    f.write(query)
+    f.close()
 
 def populate_category():
     with open("category.yaml", "r") as file_stream:
         instances = yaml.safe_load(file_stream)
     
+    query = "INSERT INTO category (idCategory, name, descripton, tax)\n\tVALUES"
+
     execute_query(connection, "DELETE FROM category")
     for pk, key in enumerate(instances, start=1):
         instance = instances[key]
-        sql = f"INSERT INTO category VALUES ('{pk}', '{instance['name']}', '{instance['description']}', '{instance['tax']}')"
-        execute_query(connection, sql)
+        query += f"\n\t('{pk}', '{instance['name']}', '{instance['description']}', '{instance['tax']}'),"
+
+    query = query[:-1] + ";\n\n"
+    execute_query(connection, query)
+    write_on_file(query)
+
 
 def populate_item():
     with open("item.yaml", "r") as file_stream:
         instances = yaml.safe_load(file_stream)
     
+    query = "INSERT INTO item (idItem, name, description, stockNr, priceBuy, priceSell, category)\n\tVALUES"
+
     execute_query(connection, "DELETE FROM Item")
     for pk, key in enumerate(instances, start=1):
         instance = instances[key]
-        sql = f"INSERT INTO Item VALUES ('{pk}', '{instance['name']}', '{instance['desc']}', '{instance['stockNr']}', '{instance['priceBuy']}', '{instance['priceSell']}', '{instance['category']}')"
-        execute_query(connection, sql)
+        query += f"\n\t('{pk}', '{instance['name']}', '{instance['desc']}', '{instance['stockNr']}', '{instance['priceBuy']}', '{instance['priceSell']}', '{instance['category']}'),"
+    
+    query = query[:-1] + ";\n\n"
+    execute_query(connection, query)
+    write_on_file(query)
 
-
-def populate_contact(connection):
+def populate_contact():
     query = "INSERT INTO contact (idContact, name, email, phone) \n\tVALUES"
 
+    execute_query(connection, "DELETE FROM contact")
     for i in range(1,31):
         name = fake.name()
         email = fake.email()
         phone_nr = random.randint(910000000,969999999)
-        if i <= 29:
-           query += f"\n\t('{i}', '{name}', '{email}', '{phone_nr}'),"
-        else:
-           query += f"\n\t('{i}', '{name}', '{email}', '{phone_nr}');\n\n"
     
-
-    query = ''.join(query)
-    #print(query)
-
+        query += f"\n\t('{i}', '{name}', '{email}', '{phone_nr}'),"
+        
+    query = query[:-1] + ";\n\n"
     execute_query(connection, query)
-    read_query(connection, "SELECT * FROM contact")
-    execute_query(connection, "DELETE FROM contact")
-
-
-    f = open("population_script.txt", "a")
-    f.write(query)
-    f.close()
+    write_on_file(query)
     
-def populate_address(connection):
+
+def populate_address():
     print("Populating address table ............")
 
-    query = ["INSERT INTO address (idAdress, street, zipCode, city)", "\n\tVALUES"]
-    # ex:  ('1', 'Rua de Santa Marta', '4750-428', 'Fafe')
-    for i in range(1, 10):
-        query.append(f"\n\t('{i}', '{fake.street_address()}', '{fake.postcode()}', '{fake.city()}'), ")
-
-    query.append(f"\n\t('{i+1}', '{fake.street_address()}', '{fake.postcode()}', '{fake.city()}');\n\n")
-
-    query = ''.join(query)
-
-    print(query)
-    execute_query(connection, query)
-    read_query(connection, "SELECT * FROM address")
-    execute_query(connection, "DELETE FROM address")
-
-    f = open('population_script.txt', 'a')
-    f.write(query)
-    f.close()
-
-    return connection 
+    query = "INSERT INTO address (idAdress, street, zipCode, city)\n\tVALUES"
     
+    execute_query(connection, "DELETE FROM address")
+    for i in range(1, 10):
+        query += f"\n\t('{i}', '{fake.street_address()}', '{fake.postcode()}', '{fake.city()}'),"
+
+    query = query[:-1] + ";\n\n"
+    execute_query(connection, query)
+    write_on_file(query)    
 
 
-
-
+populate_contact()
+populate_address()
 populate_category()
 populate_item()
 
-populate_contact(connection)
-populate_address(connection)
 
