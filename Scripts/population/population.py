d204a07b43d6d7638cc964512bcabe3c559d5dfa
Ioannis Nezis <ioannis@nezis.de>
2022-12-30 17:56:19 +0100

    adding population for Item

--- Scripts/population/population.py
+++ Scripts/population/population.py
@@ -15,11 +15,22 @@ def execute_query(connection, query):
 
 def pupulate_category():
     with open("category.yaml", "r") as file_stream:
-        categorys = yaml.safe_load(file_stream)
-        execute_query(connection, "DELETE FROM category")
-    for pk, key in enumerate(categorys):
-        cat = categorys[key]
-        sql = f"INSERT INTO category VALUES ('{pk}', '{cat['name']}', '{cat['description']}', '{cat['tax']}')"
+        instances = yaml.safe_load(file_stream)
+    
+    execute_query(connection, "DELETE FROM category")
+    for pk, key in enumerate(instances, start=1):
+        instance = instances[key]
+        sql = f"INSERT INTO category VALUES ('{pk}', '{instance['name']}', '{instance['description']}', '{instance['tax']}')"
+        execute_query(connection, sql)
+
+def pupulate_item():
+    with open("item.yaml", "r") as file_stream:
+        instances = yaml.safe_load(file_stream)
+    
+    execute_query(connection, "DELETE FROM Item")
+    for pk, key in enumerate(instances, start=1):
+        instance = instances[key]
+        sql = f"INSERT INTO Item VALUES ('{pk}', '{instance['name']}', '{instance['desc']}', '{instance['stockNr']}', '{instance['priceBuy']}', '{instance['priceSell']}', '{instance['category']}')"
         execute_query(connection, sql)
 
 def read_query(connection, query):
@@ -37,5 +48,6 @@ def read_query(connection, query):
 connection = create_db_connection("localhost", "root", argv[1], "dorlux")
 
 pupulate_category()
+pupulate_item()
 read_query(connection, "SELECT * FROM Address")
 
