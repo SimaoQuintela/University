faadee7301ddd548dece5f5156042c41941666e5
Simao Quintela <simaopedroquintela@hotmail.com>
2023-01-02 14:52:08 +0000

    employee, order and order_has_items

--- Scripts/population/population.py
+++ Scripts/population/population.py
@@ -3,7 +3,7 @@ import pandas as pd
 from faker import Faker
 from sys import argv
 from connection import create_db_connection, Error
-import random
+from random import randint, shuffle
 from faker import Faker
 from query_actions import read_query, execute_query
 fake = Faker(["pt_PT"])
@@ -20,10 +20,10 @@ def write_on_file(query):
 def populate_client():
     query = "INSERT INTO client (idClient, VAT, contact) \n\tVALUES \n\t"
     listaux = list(range(1,31))
-    random.shuffle(listaux)
+    shuffle(listaux)
     execute_query(connection, "DELETE FROM client")
     for i in range(1,31):
-        NIF = random.randint(100000000,999999999)
+        NIF = randint(100000000,999999999)
         fk = listaux[i-1]
         if i<=29:
           query += f"({i}, {NIF}, {fk}), \n\t"
@@ -33,29 +33,30 @@ def populate_client():
     write_on_file(query)
 
 def populate_client_has_address():
-    query = "INSERT INTO client_has_address (Client_idClient, Address_idAdress) \n\tVALUES \n\t"
+    query = "INSERT INTO client_has_address (Client_idClient, Address_idAdress)\n\tVALUES \n\t"
     iniCliente = 1
     fimCliente = 10
     execute_query(connection, "DELETE FROM client_has_address")
     for j in range(1,4):        #1-10    11-20    21-30       #10 20 30
         clientes = list(range(iniCliente, fimCliente))
         moradas = list(range(1,10))
-        random.shuffle(clientes)
-        random.shuffle(moradas)
+        shuffle(clientes)
+        shuffle(moradas)
         
 
         for i in range(9):
             query += f"({clientes[i]}, {moradas[i]}),\n\t"
         
-        y = random.randint(0,8)
+        y = randint(0,8)
 
-        if j == 3:
-           query += f"({j*10}, {moradas[y]});\n\n"
-        else:
-           query += f"({j*10}, {moradas[y]}),\n\t"
+        
+        query += f"({j*10}, {moradas[y]}),"
 
         iniCliente += 10
         fimCliente += 10
+    
+    query = query[:-1] + ";\n\n"
+
     execute_query(connection, query)
     write_on_file(query)
     
@@ -93,43 +94,88 @@ def populate_item():
     write_on_file(query)
 
 def populate_contact():
-    query = "INSERT INTO contact (idContact, name, email, phone) \n\tVALUES"
-    
+    query = "INSERT INTO contact (idContact, name, email, phone) \n\tVALUES"   
 
     execute_query(connection, "DELETE FROM contact")
     for i in range(1,31):
         name = fake.name()
         email = fake.email()
-        phone_nr = random.randint(910000000,969999999)
+        phone_nr = randint(910000000,969999999)
     
         query += f"\n\t('{i}', '{name}', '{email}', '{phone_nr}'),"
         
     query = query[:-1] + ";\n\n"
+    print(query)
     execute_query(connection, query)
     write_on_file(query)
     
 
 def populate_address():
-    print("Populating address table ............")
-
     query = "INSERT INTO address (idAdress, street, zipCode, city)\n\tVALUES"
     
     execute_query(connection, "DELETE FROM address")
     for i in range(1, 10):
         query += f"\n\t('{i}', '{fake.street_address()}', '{fake.postcode()}', '{fake.city()}'),"
 
+
     query = query[:-1] + ";\n\n"
     execute_query(connection, query)
-    write_on_file(query)    
+    write_on_file(query)
+
+def populate_order():
+
+    query = "INSERT INTO dorlux.Order (idOrder, status, shippingPrice, orderDate, dorlux.order.upDate, Employee, client_idClient)\n\tVALUES"
+
+    for i in range(1, 51):
+        query += f"\n\t('{i}', 'PENDING', '{randint(0,5)}.{randint(0,99)}', NOW(), NOW(), '{randint(1,3)}', '{randint(1,30)}'),"
+
+    query = query[:-1] + ";\n\n"
+    execute_query(connection, query)
+    write_on_file(query)
+
+
+def populate_employee():
+
+    query = "INSERT INTO employee (idEmployee, manager, salary, contact) VALUES\n\t"
+
+    # Mrs. Dores GIGACHAD
+    query += "('1', '1', '1500', '1'),\n\t"
+    # Other 2 employees
+    query += "('2', '1', '1000', '10'),\n\t('3', '1', '1000', '20');\n\n"
+
+    execute_query(connection, query)
+    write_on_file(query)
+
+def populate_order_has_item():
+    listaux = list(range(1,51))
+    items = list(range(1,4))
+    shuffle(listaux)
+    query = "INSERT INTO order_has_item (Order_idOrder, Item_idItem, amount) VALUES"
+
+    for i in range(1, 51):
+        order = listaux[i-1]
+        item = items[randint(0,2)]
+        query += f"\n\t('{order}', '{item}', '{randint(1, 20)}'),"
+
+    query = query[:-1] + ";\n\n"
+
+    execute_query(connection, query)
+    write_on_file(query)
 
 
 
-populate_contact()
-populate_client()
+# no relation tables
+'''
 populate_address()
-populate_client_has_address()
+populate_contact()
 populate_category()
+
+# relation tables
+populate_client()
+populate_client_has_address()
 populate_item()
+populate_employee()
 
-
-    
+populate_order()
+populate_order_has_item()
+'''
