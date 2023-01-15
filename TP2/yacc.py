ad8abfb86788512e9f91165cc9aea29b47e4ea05
David Machado <machado.david190@gmail.com>
2023-01-15 15:49:47 +0000

    Assert and yacc changes

--- TP2/yacc.py
+++ TP2/yacc.py
@@ -9,51 +9,40 @@ def p_Programa_Init(p):
     parser.assembly = f"{p[1]}"
 
 def p_Programa(p):
-    "Programa : Corpo"
-    p[0] = f'START\n{p[1]}STOP'
-
-def p_Programa_Decls(p):
-    "Programa : Decls Corpo"
-    p[0] = f'{p[1]}START\n{p[2]}STOP'
+    """Programa : Corpo
+                | Decls Corpo"""
+    decls = f'{p[1]}\n' if len(p) == 3 else ''
+    p[0] = f'{decls}START\n{p[len(p) - 1]}STOP'
 
 # ---------------- Corpo ----------------
 def p_Corpo(p):
-    "Corpo : Proc Newline"
-    p[0] = f'{p[1]}{p[2]}'
+    """Corpo : Proc Newline
+             | Corpo Proc Newline
+             | Newline"""
+    p[0] = "".join(p[1:])
 
-def p_Corpo_Proc(p):
-    "Corpo : Corpo Proc Newline"
-    p[0] = f'{p[1]}{p[2]}{p[3]}'
-
-def p_Corpo_Newline(p):
-    "Corpo : Newline"
-    p[0] = f'{p[1]}'
+# ---------------- Empty ----------------
+def p_Empty(p):
+    "Empty : "
+    pass
 
 # ---------------- Newline ----------------
-def p_Newline_Empty(p):
-    "Newline : "
-    p[0] = ''
-
 def p_Newline(p):
-    "Newline : NEWLINE"
+    """Newline : NEWLINE
+               | Empty"""
+    p.parser.lineno += 1
     p[0] = ''
 
 # ---------------- Indentation ----------------
-def p_Dedent_Rec(p):
-    "Dedent : Dedent DEDENT"
-    p[0] = f'{p[0]}{p[1]}'
-
-def p_Dedent_Empty(p):
-    "Dedent : "
+def p_Dedent(p):
+    """Dedent : Dedent DEDENT
+              | Empty"""
     p[0] = ''
 
 # ---------------- Declarations ----------------
 def p_Decls(p):
-    "Decls : Decl Newline"
-    p[0] = f'{p[1]}{p[2]}'
-
-def p_Decls_Recursiva(p):
-    "Decls : Decls Decl Newline"
+    """Decls : Decl Newline
+             | Decls Decl Newline"""
     p[0] = f'{p[1]}{p[2]}'
 
 def p_Decl_Int(p):
@@ -89,22 +78,6 @@ def p_Decl_Int_Input(p):
         print("Erro variável já inicializada")
         parser.success = False
 
-def p_DEF(p):
-    "Decl : Def"
-    p[0] = f'{p[1]}'
-
-def p_Def(p):
-    "Def : DEF ID COLON Newline INDENT Corpo DEDENT"
-    name = f'function{len(p.parser.functions)}'
-    p[0] = f'JUMP {name}Ignore\n{name}:\n{p[6]}RETURN\n{name}Ignore:\n\n'
-    p.parser.functions[p[2]+"()"] = name
-
-def p_Def_Decls(p):
-    "Def : DEF ID COLON Newline INDENT Decls Corpo DEDENT"
-    name = f'function{len(p.parser.functions)}'
-    p[0] = f'JUMP {name}Ignore\n{name}:\n{p[6]}{p[7]}RETURN\n{name}Ignore:\n\n'
-    p.parser.functions[p[2]+"()"] = name
-    
 def p_Decl_Int_Array_Val(p):
     "Decl : INTDec ID LSQBRACKET NUM RSQBRACKET ATRIB ArrayValues"
     if p[2] not in p.parser.arrays:
@@ -127,8 +100,20 @@ def p_Decl_Int_Array(p):
         print("Array já inicializado")
         parser.success = False
 
+def p_DEF(p):
+    "Decl : Def"
+    p[0] = f'{p[1]}'
 
+# ---------------- Functions ----------------
+def p_Def(p):
+    """Def : DEF ID COLON Newline INDENT Corpo DEDENT
+           | DEF ID COLON Newline INDENT Decls Corpo DEDENT"""
+    name = f'function{len(p.parser.functions)}'
+    body = "".join(p[6:len(p)-1])
+    p[0] = f'JUMP {name}Ignore\n{name}:\n{body}RETURN\n{name}Ignore:\n\n'
+    p.parser.functions[p[2]+"()"] = name
 
+# ---------------- Arrays ----------------
 def p_ArrayValues(p):
     "ArrayValues : LCURLBRACKET ArrayIntValues RCURLBRACKET"
     p[0]  = f'{p[2]}'
@@ -144,37 +129,26 @@ def p_ArrayIntValues(p):
     p.parser.gp += 1
 
 # ---------------- Procedures ----------------
-def p_Proc_Atrib(p):
-    "Proc : Atrib"
+def p_Proc(p):
+    """Proc : Atrib
+            | Print
+            | If
+            | Cycle
+            | Call
+            | Assert"""
     p[0] = p[1]
 
-def p_Proc_Print(p):
-    "Proc : Print"
-    p[0] = f'{p[1]}'
-
-def p_Proc_If(p):
-    "Proc : If"
-    p[0] = f'{p[1]}'
-
-def p_Cycle(p):
-    "Proc : Cycle"
-    p[0] = f'{p[1]}'
-
-def p_Cycle_DoWhile(p):
-    "Cycle : DoWhile"
-    p[0] = f'{p[1]}'
-
-def p_Cycle_While(p):
-    "Cycle : While"
-    p[0] = f'{p[1]}'
-
-def p_Proc_Call(p):
-    "Proc : Call"
-    p[0] = f'{p[1]}'
-
+# ---------------- Function calls ----------------
 def p_Call(p):
     "Call : CALL"
     p[0] = f'PUSHA {p.parser.functions[p[1]]}\nCALL\n'
+
+# ---------------- Asserts ----------------
+def p_Assert(p):
+    "Assert : ASSERT LCPARENT Cond RCPARENT"
+    error_message = f'ERR "False assertion in line {p.parser.lineno}"'
+    p[0] = f'{p[3]}JZ label{p.parser.labels}\nJUMP label{p.parser.labels}f\nlabel{p.parser.labels}: NOP\n{error_message}\nlabel{p.parser.labels}f: NOP\n'
+    p.parser.labels += 1
 
 # ---------------- If Else - Flux Control ----------------
 def p_If(p):
@@ -188,6 +162,11 @@ def p_If_Else(p):
     p.parser.labels += 1
 
 # ---------------- Cycles - Flux Control ----------------
+def p_Cycle(p):
+    """Cycle : While
+             | DoWhile"""
+    p[0] = p[1]
+
 def p_Do_While(p):
     "DoWhile : DO COLON Newline INDENT Corpo Dedent WHILE LCPARENT Cond RCPARENT NEWLINE"
     p[0] = f'label{p.parser.labels}:\n{p[5]}{p[9]}NOT\nJZ label{p.parser.labels}\n'
@@ -199,37 +178,27 @@ def p_While(p):
     p.parser.labels += 1
 
 # ---------------- Conditions ----------------
-def p_Cond_GT(p):
-    "Cond : Expr GT Expr"
-    p[0] = f'{p[1]}{p[3]}SUP\n'
+condition_map ={
+    ">": "SUP\n",
+    "<": "INF\n",
+    ">=": "SUPEQ\n",
+    "<=": "INFEQ\n",
+    "==": "EQUAL\n",
+    "/=": "EQUAL\nNOT\n",
+    "or": "ADD\nPUSHI 1\nSUPEQ\n",
+    "and": "ADD\nPUSHI 2\nSUPEQ\n",
+}
 
-def p_Cond_LT(p):
-    "Cond : Expr LT Expr"
-    p[0] = f'{p[1]}{p[3]}INF\n'
-
-def p_Cond_GEQ(p):
-    "Cond : Expr GEQ Expr"
-    p[0] = f'{p[1]}{p[3]}SUPEQ\n'
-
-def p_Cond_LEQ(p):
-    "Cond : Expr LEQ Expr"
-    p[0] = f'{p[1]}{p[3]}INFEQ\n'
-
-def p_Cond_EQUIV(p):
-    "Cond : Expr EQUIV Expr"
-    p[0] = f'{p[1]}{p[3]}EQUAL\n'
-
-def p_Cond_NEQ(p):
-    "Cond : Expr NEQ Expr"
-    p[0] = f'{p[1]}{p[3]}EQUAL\nNOT\n'
-
-def p_Cond_OR(p):
-    "Cond : Expr OR Expr"
-    p[0] = f'{p[1]}{p[3]}ADD\nPUSHI 1\nSUPEQ\n'
-
-def p_Cond_AND(p):
-    "Cond : Expr AND Expr"
-    p[0] = f'{p[1]}{p[3]}ADD\nPUSHI 2\nSUPEQ\n'
+def p_Cond(p):
+    """Cond : Expr GT Expr
+            | Expr LT Expr
+            | Expr GEQ Expr
+            | Expr LEQ Expr
+            | Expr EQUIV Expr
+            | Expr NEQ Expr
+            | Expr OR Expr
+            | Expr AND Expr"""
+    p[0] = f'{p[1]}{p[3]}{condition_map[p[2]]}'
 
 def p_Cond_NOT(p):
     "Cond : NOT Cond"
@@ -252,19 +221,18 @@ def p_Atrib_Input(p):
         print(f"Erro, variável {p[1]} não inicializada")
         parser.success = False
 
-def p_Atrib_INC(p):
-    "Atrib : ID INC"
-    p[0] = f'PUSHG {p.parser.registers.get(p[1])}\nPUSHI 1\nADD\nSTOREG {p.parser.registers.get(p[1])}\n'
+def p_Atrib_INC_DEC(p):
+    """Atrib : ID INC
+             | ID DEC"""
+    op = "ADD" if p[2] == "++" else "SUB"
+    p[0] = f'PUSHG {p.parser.registers.get(p[1])}\nPUSHI 1\n{op}\nSTOREG {p.parser.registers.get(p[1])}\n'
 
-def p_Atrib_DEC(p):
-    "Atrib : ID DEC"
-    p[0] = f'PUSHG {p.parser.registers.get(p[1])}\nPUSHI 1\nSUB\nSTOREG {p.parser.registers.get(p[1])}\n'
+
 def p_Atrib_Array(p):
     "Atrib : ID LSQBRACKET Expr RSQBRACKET ATRIB Expr"
     if p[1] in p.parser.registers:
         if p[1] in p.parser.arrays:
             p[0] = f'PUSHGP\nPUSHI {p.parser.registers.get(p[1])[0]}\nPADD\n{p[3]}{p[6]}STOREN\n'
-
         else:
             print(f"Erro: Variável {p[1]} não é um array.")
             parser.success = False
@@ -272,67 +240,54 @@ def p_Atrib_Array(p):
         print("Erro: Variável não definida.")
         parser.success = False
 
-
-
 # ---------------- Print ----------------
 def p_Print_NonFormatted(p):
     "Print : NonFormatted"
-    p[0] = f'{p[1]}'
+    p[0] = p[1]
 
 def p_NonFormatted(p):
     "NonFormatted : PRINT LCPARENT Argument RCPARENT"
-    p[0] = f'{p[3]}'
+    p[0] = p[3]
 
 def p_Argument_String(p):
     "Argument : String"
-    p[0] = f'{p[1]}'
+    p[0] = p[1]
 
 def p_Argument_Expr(p):
     "Argument : Expr"
     p[0] = f'{p[1]}WRITEI\nWRITELN\n'
 
 # ---------------- Expressions ----------------
-def p_Expr_Var(p):
-    "Expr : Var"
-    p[0] = p[1]
-
-def p_Expr_INC(p):
-    "Expr : ExprIncDec"
+def p_Expr_Var_Inc(p):
+    """Expr : Var
+            | ExprIncDec"""
     p[0] = p[1]
 
 def p_Expr_Num(p):
     "Expr : NUM"
     p[0] = f'PUSHI {p[1]}\n'
 
-def p_Expr_Soma(p):
-    "Expr : Expr SUM Expr"
-    p[0] = f'{p[1]}{p[3]}ADD\n'
+arith_map = {
+    "+": "ADD\n",
+    "-": "SUB\n",
+    "*": "MUL\n",
+    "/": "DIV\n",
+    "%": "MOD\n",
+}
+def p_Expr_Arith(p):
+    """Expr : Expr SUM Expr
+            | Expr SUB Expr
+            | Expr MULT Expr
+            | Expr DIV Expr
+            | Expr MOD Expr"""
+    p[0] = f'{p[1]}{p[3]}{arith_map[p[2]]}'
 
-def p_Expr_Sub(p):
-    "Expr : Expr  SUB Expr"
-    p[0] = f'{p[1]}{p[3]}SUB\n'
-
-def p_Expr_Mult(p):
-    "Expr : Expr  MULT Expr"
-    p[0] = f'{p[1]}{p[3]}MUL\n'
-
-def p_Expr_Div(p):
-    "Expr : Expr  DIV Expr"
-    p[0] = f'{p[1]}{p[3]}DIV\n'
-
-def p_Expr_Mod(p):
-    "Expr : Expr MOD Expr"
-    p[0] = f'{p[1]}{p[3]}MOD\n'
-
-def p_Expr_Inc(p):
-    "ExprIncDec : ID INC"
+def p_Expr_Inc_Dec(p):
+    """ExprIncDec : ID INC
+                  | ID DEC"""
+    op = "ADD" if p[2] == "++" else "SUB"
     var_gp_index = p.parser.registers.get(p[1])
-    p[0] = f'PUSHG {var_gp_index}\nPUSHI 1\nADD\nSTOREG {var_gp_index}\nPUSHG {var_gp_index}\n'
-
-def p_Expr_Dec(p):
-    "ExprIncDec : ID DEC"
-    var_gp_index = p.parser.registers.get(p[1])
-    p[0] = f'PUSHG {var_gp_index}\nPUSHI 1\nSUB\nSTOREG {var_gp_index}\nPUSHG {var_gp_index}\n'
+    p[0] = f'PUSHG {var_gp_index}\nPUSHI 1\n{op}\nSTOREG {var_gp_index}\nPUSHG {var_gp_index}\n'
 
 # ---------------- Vars -----------------
 def p_Var(p):
@@ -348,7 +303,7 @@ def p_Var(p):
         print("Erro: Variável não definida")
 
 def p_Var_Array(p):
-    "Var    : ID LSQBRACKET Expr RSQBRACKET"
+    "Var : ID LSQBRACKET Expr RSQBRACKET"
     if p[1] in p.parser.registers:
         if p[1] not in p.parser.ints:
             p[0] = f'PUSHGP\nPUSHI {p.parser.registers.get(p[1])[0]}\nPADD\n{p[3]}LOADN\n'
@@ -370,9 +325,8 @@ def p_String(p):
     p[0] = f'PUSHS "{p[2]}"\nWRITES\n'
 
 def p_String_Empty(p): 
-    "String :  "
+    "String : Empty"
     p[0] = ''
-
 
 # ---------------- Errors ----------------
 def p_error(p):
@@ -409,6 +363,7 @@ parser.ints = []
 parser.arrays = []
 parser.labels = 0
 parser.functions = {}
+parser.lineno = 1
 
 with open(f"tests/{sys.argv[1]}.plc") as f:
     content = f.read()
